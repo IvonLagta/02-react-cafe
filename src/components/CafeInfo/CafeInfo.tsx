f41288feb2e4374fc4cf1f692951cb2ab1d73884
IvonLagta <32240085+IvonLagta@users.noreply.github.com>
2026-03-22 19:51:52 +0000

    Update CafeInfo.tsx

--- src/components/CafeInfo/CafeInfo.tsx
+++ src/components/CafeInfo/CafeInfo.tsx
@@ -4,7 +4,7 @@ export default function CafeInfo() {
   return (
     <>
       <div className={css.container}>
-        <h1 className={css.title}>Rejected qwerty</h1>
+        <h1 className={css.title}>Sip Happens Café</h1>
         <p className={css.description}>
           Please rate our service by selecting one of the options below.
         </p>
