3ced98d2b2a47d0df001154b6bd041fb8fca26d4
IvonLagta <32240085+IvonLagta@users.noreply.github.com>
2026-03-22 19:54:01 +0000

    Update Notification.tsx

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,5 +1,5 @@
 import css from "./Notification.module.css";
 
 export default function Notification() {
-  return <p className={css.message}>We'd like to hear your oppinion</p>;
+  return <p className={css.message}>No feedback yet</p>;
 }
